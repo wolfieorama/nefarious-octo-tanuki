75551bbe78a98ba3189b5ffdb6bb10d049015953
wolfieorama <wachirawamunyi@gmail.com>
2015-10-29 17:33:01 +0300

    mongodb yml setup

--- Gemfile
+++ Gemfile
@@ -26,7 +26,6 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'mongoid', '~> 5.0'
 
 gem 'bson_ext'
-
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,7 @@ class Car
   include Mongoid::Document
   field :name, type: String
   field :reg_no, type: String
+  field :driver, type: String
+  field :bought_on, type: Date
+  field :location, :type => String
 end
